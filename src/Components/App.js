bdbbcb0d717f8c68b1e9f8219505f29207e37e47
Cole <colejcrutcher@gmail.com>
2021-03-10 10:29:23 -0600

    Css base layout

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import '../Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="top-section">
+        <div id="title">Memory</div>
+        <div id="score">Score</div>
+      </div>
+      <div id="bottom-section">
+        <div id="cards">Cards</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
